2dd2676386e78a75b4f7aa395131869a68775219
Karlee Fidek <kdf131@uregina.ca>
2019-09-29 00:10:25 -0600

    Update ListElement.java

--- ListElement.java
+++ ListElement.java
@@ -1,19 +1,45 @@
-public class ListElement{
+/*
+Karlee Fidek
+200386366
+
+ENSE 374 Lab
+Assignment 2
+October 3, 2019
+
+class adapted from douglatr
+ */
+ 
+public class ListElement {
+	
+	
 	private ListElement next;
 	private int data;
 	
-	public ListElement();
+	
+	public ListElement()
 	{
-		this.data = 0;
 		this.next = null;
-		this.previous = null;
+		this.data = 0;
 	}
 	
-	public void setData(int data){
+	
+	public void setData(int data)
+	{
 		this.data = data;
 	}
 	
-	public int getData(){
+	public int getData()
+	{
 		return this.data;
+	}
+	
+	public void setNext(ListElement next)
+	{
+		this.next = next;
+	}
+	
+	public ListElement getNext()
+	{
+		return this.next;
 	}
 }
