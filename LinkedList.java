93617cccccd38367f36544a7ff51d5c8503b53a4
Karlee Fidek <kdf131@uregina.ca>
2019-10-03 15:15:14 -0600

    added comments

--- LinkedList.java
+++ LinkedList.java
@@ -8,18 +8,21 @@ October 3, 2019
 */
 
 public class LinkedList{
-    
+    //list element at beginning of list
     ListElement head = new ListElement();
     
+    //initial constructor
     public LinkedList()
     {
         head = null;
     }
     
+    //adds given element to list
     public void addElement(ListElement le)
     {
         if(head == null)
         {
+            //no elements, adds element at beginning
             head = le;
         }
         
@@ -29,25 +32,30 @@ public class LinkedList{
             
             while(currPtr.getNext() != null)
             {
+                //loops to the last element in the list
                 currPtr = currPtr.getNext();
             }
             
+            //links new element to the last element in the list
             currPtr.setNext(le);
         }
     }
     
     public ListElement getElement(int index)
     {   
+        //dummy element used if wanted element DNE
         ListElement notFound = new ListElement();
         
         if(head == null)
         {
+            //no elements, return dummy element
             return notFound;
         }
         else
         {
             ListElement tempPtr = head;
             
+            //searches till end of list to find element with given value
             while((tempPtr.getNext() != null) && (tempPtr.getData() != index))
             {
                 tempPtr = tempPtr.getNext();
@@ -55,10 +63,12 @@ public class LinkedList{
             
             if(tempPtr.getData() == index)
             {
+                //if element exists in list, return it
                 return tempPtr;
             }
             else
             {
+                //element DNE in list, return dummy element
                 return notFound;
             }
         }
@@ -66,38 +76,48 @@ public class LinkedList{
     
     public ListElement deleteElement(int index)
     {
+        //dummy element to be used if element DNE
         ListElement notFound = new ListElement();
         ListElement tempPtr = head;
         
         if(head == null)
         {
+            //no elements, return dummy element
             return notFound;
         }
         else
         {
             if(head.getData() == index)
             {
+                //return head if it is the same as given value
                 head = head.getNext();
                 return tempPtr;
             }
             else
             {
+                //if element is not in head, look through rest of list
                 tempPtr = head.getNext();
                 ListElement prevPtr = head;
         
                 while((tempPtr.getNext() != null) && (tempPtr.getData() != index))
                 {
+                    //searches rest of list for element with given value
                     prevPtr = tempPtr;                
                     tempPtr = tempPtr.getNext();
                 }   
         
                 if(tempPtr.getData() == index)
                 {
+                    //if element was found, link the previous element
+                    //to the next element after the one you want to delete
+                    //which removes the element from the list
                     prevPtr.setNext(tempPtr.getNext());
+                    //return deleted element
                     return tempPtr;
                 }
                 else
                 {
+                    //not found, return dummy element
                     return notFound;
                 }   
             }
@@ -108,10 +128,13 @@ public class LinkedList{
     {
         if(head != null)
         {
+            //starts at beginning of list - head
             ListElement tempPtr = head;
         
             while(tempPtr.getNext() != null)
             {
+                //outputs value of each element and then progresses
+                //to next element
                 System.out.print(tempPtr.getData() + " --> ");
                 tempPtr = tempPtr.getNext();
             } 
@@ -120,6 +143,7 @@ public class LinkedList{
         }
         else
         {
+            //no elements in list, print default statement
             System.out.print("No list to print.");
         }
         
@@ -128,6 +152,7 @@ public class LinkedList{
     
     public static void main (String[] args)
     {
+        //creates instance of linked lists and multiple list elements
         LinkedList list = new LinkedList();
         ListElement e1 = new ListElement();
         ListElement e2 = new ListElement();
@@ -135,33 +160,45 @@ public class LinkedList{
         ListElement e4 = new ListElement();
         ListElement e5 = new ListElement();
         
+        //sets value of element and adds it to the linked list
         e1.setData(5);
         list.addElement(e1);
         
+        //sets value of element and adds it to the linked list
         e2.setData(2);
         list.addElement(e2);
         
+        //sets value of element and adds it to the linked list
         e3.setData(9);
         list.addElement(e3);
         
+        //prints linked list from head
         list.printLinkedListHead();
         
+        //deletes element from list
         System.out.println("Delete element with value of " + list.deleteElement(5).getData() + ". . .");
         
+        //prints linked list from head
         list.printLinkedListHead();
         
+        //sets value of element and adds it to the linked list
         e4.setData(15);
         list.addElement(e4);
         
+        //sets value of element and adds it to the linked list
         e5.setData(4);
         list.addElement(e5);
         
+        //prints linked list from head
         list.printLinkedListHead();
         
+        //deletes element from list
         System.out.println("Delete element with value of " + list.deleteElement(15).getData() + ". . .");
         
+        //prints linked list from head
         list.printLinkedListHead();
         
+        //retrieves element from list
         System.out.println("Retrieved element with value of " + list.getElement(9).getData() + " from the list");
     }
 }
--- ListElement.java
+++ ListElement.java
@@ -11,31 +11,36 @@ class adapted from douglatr
  
 public class ListElement {
 	
+	//list element to link to another element, data to hold a value
 	private ListElement next;
 	private int data;
 	
-	
+	//initial constructor	
 	public ListElement()
 	{
 		this.next = null;
 		this.data = 0;
 	}
 	
+	//sets data to given value
 	public void setData(int data)
 	{
 		this.data = data;
 	}
 	
+	//gets value from element
 	public int getData()
 	{
 		return this.data;
 	}
 	
+	//sets link to given element
 	public void setNext(ListElement next)
 	{
 		this.next = next;
 	}
 	
+	//gets linked element
 	public ListElement getNext()
 	{
 		return this.next;
